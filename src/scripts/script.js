538244923c7382c4641fe87eedcde1c3acc78797
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2019-03-08 23:05:32 +0700

    Fix button waves

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,12 +3,47 @@ $(document).ready(function () {
         var scroll = $(window).scrollTop();
         var opacity = 0;
         if (scroll < 100) {
-            opacity = scroll / 100 ;
+            opacity = scroll / 100;
             $("#b-page-header").css('background-color', 'rgba(75, 193, 210, ' + opacity + ')');
         } else {
             opacity = 1;
             $("#b-page-header").css('background-color', 'rgba(75, 193, 210, ' + opacity + ')')
         }
     })
+    $(".b-link").click(function (e) {
+
+        // Remove any old one
+        $(".ripple").remove();
+
+        // Setup
+        var posX = $(this).offset().left,
+            posY = $(this).offset().top,
+            buttonWidth = $(this).width(),
+            buttonHeight = $(this).height();
+
+        // Add the element
+        $(this).prepend("<span class='ripple'></span>");
+
+
+        // Make it round!
+        if (buttonWidth >= buttonHeight) {
+            buttonHeight = buttonWidth;
+        } else {
+            buttonWidth = buttonHeight;
+        }
+
+        // Get the center of the element
+        var x = e.pageX - posX - buttonWidth / 2;
+        var y = e.pageY - posY - buttonHeight / 2;
+
+
+        // Add the ripples CSS and start the animation
+        $(".ripple").css({
+            width: buttonWidth,
+            height: buttonHeight,
+            top: y + 'px',
+            left: x + 'px'
+        }).addClass("rippleEffect");
+    });
 });
 
